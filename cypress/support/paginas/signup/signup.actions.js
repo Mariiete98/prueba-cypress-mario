09a3056b78892a16dadee0fbf7e7adf258b502b1
Mario <mario_zgz_98@hotmail.com>
2026-02-04 15:49:10 +0100

    Creacion pagina Signup

---
+++ cypress/support/paginas/signup/signup.actions.js
@@ -0,0 +1,21 @@
+export class SignupActions {
+    static insertarUsuario(username){
+        SignupElements.obtenerCampos.username.invoke('val', username)
+    }
+    static insertarPassword(password){
+        SignupElements.obtenerCampos.password.invoke('val', password)
+    }
+    static clickSignup(){
+        SingupElements.obtenerBotones.signup.click()
+    }
+
+    // podrian ir en otro archivo para optimizar
+
+    // se usará:
+
+    static signupCompleto(username, password){
+        this.insertarUsuario(username);
+        this.insertarPassword(password);
+        this.clickSignup();
+    }
+}
---
+++ cypress/support/paginas/signup/signup.elements.js
@@ -0,0 +1,26 @@
+export class SignupElements {
+    static get obtenerCampos() {
+        return {
+        get username() {
+            return cy.get("input#sign-username");
+        },
+        get password() {
+            return cy.get("input#sign-password");
+        },
+        };
+    }
+
+    static get obtenerBotones() {
+        return {
+        get cerrar() {
+            return cy.contains('button', 'Close');
+        },
+
+        // Selecciona un tag y el texto que contiene
+
+        get login() {
+            return cy.contains('button', 'Sign in');
+        },
+        };
+    }
+}
