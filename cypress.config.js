d46cd8e0130d493a307175d15038dd0d85094492
Mario <mario_zgz_98@hotmail.com>
2026-02-04 12:59:26 +0100

    Configuracion  de Reportes

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
@@ -6,6 +7,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
+    allowCypressEnv: true
   },
 });
